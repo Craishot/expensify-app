09e7102b03acacd867243c93af7818f089da09b0
Trentin Gillis <craishot@gmail.com>
2018-08-05 17:09:13 -0600

    Setup production webpack build\

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,62 @@
+// Import needed plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 // Require needed packages
 const path = require('path');
 
-// Export webpack config
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                // Rules for .js files
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }, 
-            {
-                // Rules for .css/.scss files
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+// Export the Webpack configuration
+module.exports = (env) => {
+    // Check we are running webpack in production mode
+    const isProduction = env === 'production';
+
+    // Setup ExtractText plugin
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    // Return cofiguration settings we want to export
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    // Rules for .js files
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                }, 
+                {
+                    // Rules for .css/.scss files
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        // Use source map depending on whether building for production or development
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
